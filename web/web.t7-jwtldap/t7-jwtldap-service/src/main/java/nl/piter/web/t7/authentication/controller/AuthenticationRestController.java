35e6f41026ac549a0b3c86a0190c2e86bf2b3c15
Piter T. de Boer <ptdeboer@gmail.com>
2025-10-04 18:17:11 +0200

    Headers.

--- web/web.t7-jwtldap/t7-jwtldap-service/src/main/java/nl/piter/web/t7/authentication/controller/AuthenticationRestController.java
+++ web/web.t7-jwtldap/t7-jwtldap-service/src/main/java/nl/piter/web/t7/authentication/controller/AuthenticationRestController.java
@@ -1,6 +1,7 @@
-/* (C-left) 2015-2025 Piter.NL - Free of use, but keep this header.
- * https://www.piter.nl/github
- * See LICENSE.txt for more details.
+/*/
+ */ (C-left) 2015-2025 Piter.NL - Free of use, but keep this header.
+ */ https://www.piter.nl/github
+ */ See LICENSE.txt for more details.
  */
 //
 package nl.piter.web.t7.authentication.controller;
